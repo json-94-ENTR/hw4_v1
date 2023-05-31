54d2e2156cd9aaf2f05becbfdda1a0978a0a3f54
Janu Sonthi <janu.sonthi@kellogg.northwestern.edu>
2023-05-31 03:51:32 +0000

    updated_sessoins_controller_and_view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
+    flash["notice"] = "Incorrect username or password"
+    redirect_to "/sessions/new"
   end
 
   def destroy
