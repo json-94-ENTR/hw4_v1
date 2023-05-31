fa3f43cb6744c7a9157d5c77877389345b85bef1
Janu Sonthi <janu.sonthi@kellogg.northwestern.edu>
2023-05-31 04:01:17 +0000

    authenticating_user_session_controller_code_v1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({"email" => params["email"]})
+    if @user
+    
+  
+    else 
     flash["notice"] = "Incorrect username or password"
     redirect_to "/sessions/new"
   end
