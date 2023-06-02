7ad5c1506e50033d8c2310b7d42033bf49a1ad6c
Janu Sonthi <janu.sonthi@kellogg.northwestern.edu>
2023-06-02 03:33:13 +0000

    fixed_rails_runner_script_code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by({"email" => params["email"]})
-  if @user
+  @user = User.find_by({"email" => params["email"]})
+    if @user
     password_the_user_typed = params["password"]
     password_in_the_database = @user["password"]
     If BCrypt::Password.new(password_in_the_database) == password_the_user_typed
-      flash["notice"] = "Welcome"
+      flash["notice"] = "Welcome!"
       redirect_to "/home"
     else 
       flash["notice"] = "Email, username, or password is unavailable. Try Again."
       redirect_to "/sessions/new"
+    end
   end
+end
 
   def destroy
   end
