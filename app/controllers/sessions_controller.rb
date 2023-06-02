706edbbbe935cf8f4b337bca224f653841754f23
Janu Sonthi <janu.sonthi@kellogg.northwestern.edu>
2023-06-02 06:02:16 +0000

    updated_sessions_controller_not_working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,16 +5,16 @@ class SessionsController < ApplicationController
   def create
   @user = User.find_by({ "email" => params["email"] })
   if @user
-    if BCrypt::Password.new(@user["password"]) == params ["password"]
+    if BCrypt::Password.new(@user["password"]) == (params ["password"])
       flash["notice"] = "Welcome! You've logged in."
       cookies["user_id"] = @user["id"]
       redirect_to "/places"
-    else 
+      else 
+        flash["notice"] = "Unsuccessful login."
+        redirect_to "/sessions/new"
+      end
+    else
       flash["notice"] = "Unsuccessful login."
       redirect_to "/sessions/new"
     end
-  else
-  flash["notice"] = "Unsuccessful login."
-  redirect_to "/sessions/new"
   end
-end
